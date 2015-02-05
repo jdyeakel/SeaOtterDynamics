fe09cb884cd61b5af3277442f948739d9e531c55
jdyeakel <jdyeakel@gmail.com>
2015-02-05 14:53:49 -0700

    fileoutlinecache in nb files

--- GenModel_Jacobian.R
+++ GenModel_Jacobian.R
@@ -1,1 +1,13 @@
 #Implement Gen Model Jacobian and analyze stability over large parameter space
+
+#Number of Resources
+N = 2
+
+#Dimensions of the Jacobian Matrix
+J = matrix(data = 0, N+2,N+2)
+
+
+
+
+
+
