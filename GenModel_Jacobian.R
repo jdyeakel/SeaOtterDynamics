9c72e38eb0b80a531975d6e92021c51f21ab0f0f
jdyeakel <jdyeakel@gmail.com>
2015-02-05 18:07:14 -0700

    initiated Jacobian cpp code

--- GenModel_Jacobian.R
+++ GenModel_Jacobian.R
@@ -6,6 +6,14 @@ N = 2
 #Dimensions of the Jacobian Matrix
 J = matrix(data = 0, N+2,N+2)
 
+#define elasticity value (if constant) or min/max (if range)
+dfdi_min = numeric(N)
+dfdi_max = numeric(N) + 2
+
+dfdc_min = 0
+dfdc_max = 1
+
+
 
 
 
--- src/PSWJacobian.cpp
+++ src/PSWJacobian.cpp
@@ -8,6 +8,36 @@ using namespace Rcpp;
 // For more on using Rcpp click the Help button on the editor toolbar
 
 // [[Rcpp::export]]
-int timesTwo(int x) {
-   return x * 2;
+double PSWJacobian(
+  int rep,
+  int N,
+  
+  ) {
+   
+   //Loop over possible food webs
+   for (int r=0; r<rep; r++) {
+     
+     //Choose values for elasticities based on previously assigned values
+     //or Min/Max ranges
+     
+     
+     
+     //Build Jacobian
+     res_diag =
+     res_offdiag =
+     
+     c_diag =
+     c_offdiag =
+     
+     p_diag =
+     p_offdiag =
+     
+     NumericMatrix Jac(N+2,N+2)
+     
+     
+     
+   }
+   
+//Calculate PSW
+   
 }
